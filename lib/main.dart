00633254ce683a7e5bbee91572c103d82ae9ab4e
David Moreno Buj <davidmb91@gmail.com>
2021-09-19 09:51:38 +0200

    Pase de argumentos con PushNamed

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:peliculas/src/pages/home_page.dart';
+import 'package:peliculas/src/pages/pelicula_detalle.dart';
 
 void main() async {
   await dotenv.load(fileName: ".env");
@@ -16,6 +17,7 @@ class MyApp extends StatelessWidget {
       initialRoute: '/',
       routes: {
         '/': (BuildContext context) => HomePage(),
+        'detalle': (BuildContext context) => PeliculaDetalle(),
       },
     );
   }
