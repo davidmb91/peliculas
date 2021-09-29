2ecc0862961fb095b9a4a854559a8b1118f73f07
David Moreno Buj <davidmb91@gmail.com>
2021-09-29 15:41:49 +0200

    Cambios silverlist con descripción repetida

--- lib/src/pages/pelicula_detalle.dart
+++ lib/src/pages/pelicula_detalle.dart
@@ -10,22 +10,23 @@ class PeliculaDetalle extends StatelessWidget {
         ModalRoute.of(context)!.settings.arguments as Pelicula;
 
     return Scaffold(
-      body: CustomScrollView(
-        slivers: [
-          _crearAppbar(pelicula),
-          SliverFixedExtentList(  // TODO: Luego borrar, esto es relleno de prueba para forzar scroll
-            itemExtent: 50.0,
-            delegate: SliverChildBuilderDelegate(
-              (BuildContext context, int index) {
-                return Container(
-                  alignment: Alignment.center,
-                  color: Colors.lightBlue[100 * (index % 9)],
-                  child: Text('Lorem ipsum $index'),
-                );
-              },
+        body: CustomScrollView(
+      slivers: [
+        _crearAppbar(pelicula),
+        SliverList(
+          delegate: SliverChildListDelegate([
+            SizedBox(
+              height: 10.0,
             ),
-          ),
-        ],
+            _posterTitulo(context, pelicula),
+            _descripcion(pelicula),
+            _descripcion(pelicula),
+            _descripcion(pelicula),
+            _descripcion(pelicula),
+            _descripcion(pelicula),
+          ]),
+        )
+      ],
     ));
   }
 
@@ -38,7 +39,10 @@ class PeliculaDetalle extends StatelessWidget {
       pinned: true,
       flexibleSpace: FlexibleSpaceBar(
         centerTitle: true,
-        title: Text(pelicula.title.toString(), style: TextStyle(color: Colors.white, fontSize: 16.0),),
+        title: Text(
+          pelicula.title.toString(),
+          style: TextStyle(color: Colors.white, fontSize: 16.0),
+        ),
         background: FadeInImage(
           image: NetworkImage(pelicula.getBackgroundImg()),
           placeholder: AssetImage('assets/img/loading.gif'),
@@ -46,7 +50,50 @@ class PeliculaDetalle extends StatelessWidget {
           fit: BoxFit.cover,
         ),
       ),
-      
+    );
+  }
+
+  Widget _posterTitulo(BuildContext context, Pelicula pelicula) {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 20.0),
+      child: Row(
+        children: [
+          ClipRRect(
+            borderRadius: BorderRadius.circular(20.0),
+            child: Image(
+              image: NetworkImage(pelicula.getPosterImg()),
+              height: 150.0,
+            ),
+          ),
+          SizedBox(width: 20.0),
+          Flexible(
+              child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(pelicula.title.toString(),
+                  style: Theme.of(context).textTheme.headline6,
+                  overflow: TextOverflow.ellipsis),
+              Text(pelicula.originalTitle.toString(),
+                  style: Theme.of(context).textTheme.subtitle1,
+                  overflow: TextOverflow.ellipsis),
+              Row(
+                children: [
+                  Icon(Icons.star_border),
+                  Text(pelicula.voteAverage.toString(),
+                      style: Theme.of(context).textTheme.subtitle1)
+                ],
+              )
+            ],
+          ))
+        ],
+      ),
+    );
+  }
+
+  Widget _descripcion(Pelicula pelicula) {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 10.0, vertical: 20.0),
+      child: Text(pelicula.overview.toString(), textAlign: TextAlign.justify),
     );
   }
 }
